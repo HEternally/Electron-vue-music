e013d2d98b2350774ef67455e0a893e2fdc55ddc
Heternally <Heternally@163.com>
2019-09-26 20:39:26 +0800

    搭建页面骨架

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
-
+    <horizontal-menu/>
+    <vertical-menu/>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <music-player/>
   </div>
 </template>
 
 <script>
+  import { mapActions } from 'vuex';
+  import horizontalMenu from '@/navMenu/horizontalMenu.vue';
+  import verticalMenu from '@/navMenu/verticalMenu.vue';
+  import musicPlayer from '@/components/musicPlayer/musicPlayer.vue';
   export default {
     name: 'music',
-    components: {},
+    components: {
+      horizontalMenu,
+      verticalMenu,
+      musicPlayer,
+    },
+    methods: {
+      ...mapActions('histroyRouter', {
+        clearAsyncHistory: 'clearAsyncHistory',
+      }),
+      init() {
+        this.$router.replace('/home');
+      },
+    },
+    created() {
+      this.clearAsyncHistory();
+      this.init();
+      window.this = this;
+    },
   };
 </script>
 
