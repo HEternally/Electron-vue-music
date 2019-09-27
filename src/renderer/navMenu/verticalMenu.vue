acde2619e281000b918e4ab12912459c11dae140
Heternally <Heternally@163.com>
2019-09-27 18:14:26 +0800

    完善侧边导航目录在嵌套路由下选中情况

--- src/renderer/navMenu/verticalMenu.vue
+++ src/renderer/navMenu/verticalMenu.vue
@@ -3,7 +3,7 @@
     <el-row class="tac">
       <el-col>
         <el-menu
-          default-active="/home"
+          :default-active="$route.path"
           class="el-menu-vertical-demo"
           @open="handleOpen"
           @close="handleClose"
@@ -12,40 +12,40 @@
           active-text-color="#D33A31"
           :router="true"
         >
-          <el-menu-item index="/home">
+          <el-menu-item index="/home" :style="fullPath === '/home' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-headset"></i>
             <span slot="title">发现音乐</span>
           </el-menu-item>
-          <el-menu-item index="/songList">
+          <el-menu-item index="/personalFM" :style="fullPath === '/personalFM' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-files"></i>
             <span slot="title">私人FM</span>
           </el-menu-item>
-          <el-menu-item index="/radio">
+          <el-menu-item index="/MV" :style="fullPath === '/MV' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-monitor"></i>
             <span slot="title">视频</span>
           </el-menu-item>
-          <el-menu-item index="4">
+          <el-menu-item index="/friends" :style="fullPath === '/friends' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-user"></i>
             <span slot="title">朋友</span>
           </el-menu-item>
           <p class="myMusic">我的音乐</p>
-          <el-menu-item index="5">
+          <el-menu-item index="/friends" :style="fullPath === '/friends' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-folder-opened"></i>
             <span slot="title">iTunes音乐</span>
           </el-menu-item>
-          <el-menu-item index="6">
+          <el-menu-item index="/friends" :style="fullPath === '/friends' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-download"></i>
             <span slot="title">下载管理</span>
           </el-menu-item>
-          <el-menu-item index="7">
+          <el-menu-item index="/friends" :style="fullPath === '/friends' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-s-promotion"></i>
             <span slot="title">我的音乐云盘</span>
           </el-menu-item>
-          <el-menu-item index="8">
+          <el-menu-item index="/friends" :style="fullPath === '/friends' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-mobile"></i>
             <span slot="title">我的电台</span>
           </el-menu-item>
-          <el-menu-item index="9">
+          <el-menu-item index="/friends" :style="fullPath === '/friends' ? 'color:#D33A31;background-color: #E2E2E2;' : 'color: #303133;background-color: #EDEDED;'">
             <i class="el-icon-star-off"></i>
             <span slot="title">我的收藏</span>
           </el-menu-item>
@@ -75,6 +75,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      fullPath: '',
+    };
+  },
   methods: {
     handleOpen(key, keyPath) {
       console.log(key, keyPath); // eslint-disable-line
@@ -83,11 +88,19 @@ export default {
       console.log(key, keyPath); // eslint-disable-line
     },
   },
+  watch: {
+    '$route'(to, from) {// eslint-disable-line
+      this.fullPath = to.matched[0].path;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
   #vertical {
+    position: absolute;
+    left:0;
+    top:0;
     display: inline-block;
     box-sizing: border-box;
     width: 200px;
@@ -95,15 +108,18 @@ export default {
     padding: 50px 0 61px 0;
     overflow-y: auto;
     background-color: #EDEDED;
-    z-index: -1;
+    z-index: 1;
     .el-menu-item {
       font-size: 16px;
       font-weight: 800;
       height: 36px;
       line-height: 36px;
       &:hover {
-        background-color:#E2E2E2 !important;
+        background-color:rgb(231, 231, 231) !important;
       }
+    }
+    .is-active {
+      background-color:#E2E2E2 !important;
     }
     .myMusic {
       padding: 20px 20px 10px;
