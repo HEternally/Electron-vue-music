01b35dd468504ad1b62285140776734d1302bbd3
Heternally <Heternally@163.com>
2019-09-30 18:36:01 +0800

    修改request接口名称

--- src/renderer/utils/request.js
+++ src/renderer/utils/request.js
@@ -1,10 +1,10 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable arrow-parens */
 import axios from 'axios';
-import store from '@/store';
+// import store from '@/store';
 
 const baseURL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000/api'
+  ? 'http://localhost:3000'
   : 'http://39.105.232.6:3000';
 
 // 创建axios实例
@@ -16,16 +16,20 @@ const instance = axios.create({
 // 添加请求拦截器
 instance.interceptors.request.use(config => {
   // 在发送请求之前做些什么
-  return config + store;
+  console.log(config);// eslint-disable-line
+  return config;
 }, error => {
   // 对请求错误做些什么
+  console.log(error);// eslint-disable-line
   return Promise.reject(error);
 });
 
 // 添加响应拦截器
 instance.interceptors.response.use(response => {
+  console.log(response);// eslint-disable-line
   return response;
 }, error => {
+  console.log(error);// eslint-disable-line
   return Promise.reject(error);
 });
 
