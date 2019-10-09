bceeb4c2a7fecb55746af59b2f7708ef2b0c9b14
Heternally <Heternally@163.com>
2019-10-09 17:29:52 +0800

    新增歌单详情页

---
+++ src/renderer/views/playList/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{id}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'playList',
+  data() {
+    return {
+      id: '',
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.id = this.$route.params.id;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
